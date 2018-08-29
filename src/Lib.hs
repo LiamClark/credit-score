14c32657dc2dc278c109e52e9de27dc76d0d58a1
LiamClark <L.A.Clark@student.tudelft.nl>
2018-08-29 16:00:14 +0200

    nme

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,8 +8,11 @@ module Lib
 import Control.Applicative
 import qualified Data.ByteString.Lazy as BL
 import Data.Csv
+import Data.Coerce
+import Data.Monoid
+import Data.Foldable
 import qualified Data.Vector as V
-
+import Data.Bifuctor
 
 someFunc :: IO ()
 someFunc = do
@@ -20,11 +23,27 @@ someFunc = do
             putStrLn $ show $ target p
 
 data CreditScore = CreditScore {
-    target :: !Bool
+     target :: !Bool
+    ,creditAmount :: !Int
 }
+
+nme :: V.Vector CreditScore -> Int -> Bool
+nme dataSet = \c -> credibleMean `dist` fromIntegral c < incredibleMeme `dist` fromIntegral c
+    where 
+         umap f = bimap f f
+         (credibleMean, incredibleMeme) = umap mean (V.partition target dataSet)
+
+mean :: V.Vector CreditScore -> Double
+mean xs =  (fromIntegral total) / (fromIntegral  (length xs))
+     where total :: Int = coerce $ foldMap (Sum . creditAmount) xs 
+
+dist :: Double -> Double -> Double
+dist = (abs .) . (-)
+
 
 instance FromNamedRecord CreditScore where
     parseNamedRecord r =  do
            classTag :: String <-  r .: "class"
-           pure $ CreditScore $ classTag == "good"
+           creditAmount <- r .: "credit_amount"
+           pure $ CreditScore (classTag == "good") creditAmount
 
