dfd397e7f0ffc9de35386a96b39ac745c26399d2
LiamClark <L.A.Clark@student.tudelft.nl>
2018-08-29 14:01:31 +0200

    dataset and parsing casava stuffies

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
+{-#LANGUAGE OverloadedStrings#-}
+
 module Lib
     ( someFunc
     ) where
 
+import Control.Applicative
+import Data.Csv
+
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data CreditScore = CreditScore {
+    target :: !Bool
+}
+
+instance FromNamedRecord CreditScore where
+    parseNamedRecord r =  do
+           classTag <-  r .: "class"
+           pure $ CreditScore $ classTag == "good"
+
